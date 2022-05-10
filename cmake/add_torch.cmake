9f1c356ac048d725ff2718fa5cbc742cfb69849f
Jb Dpy <37930951+ZodiacFRA@users.noreply.github.com>
2022-05-10 13:22:23 +0000

    Update add_torch.cmake

--- cmake/add_torch.cmake
+++ cmake/add_torch.cmake
@@ -20,28 +20,31 @@ endif()
 if (NOT torch_lib)
   message(STATUS "Downloading torch C API pre-built")
 
-  # Download
-  if (UNIX AND NOT APPLE)  # Linux
-    set(torch_url
-        "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${torch_version}%2Bcpu.zip")
-  elseif (UNIX AND APPLE)  # OSX
-    set(torch_url
-        "https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip")
-  else()                   # Windows
-    set(torch_url
-        "https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip")
+  if (UNIX AND APPLE AND ${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "arm64")
+    set(torch_url "https://anaconda.org/pytorch/pytorch/1.11.0.arm64/download/osx-arm64/pytorch-${torch_version}.arm64-py3.9_0.tar.bz2")
+    file(DOWNLOAD
+      ${torch_url}
+      ${torch_dir}/torch_cc.tar.bz2
+      SHOW_PROGRESS
+    )
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf torch_cc.tar.bz2 WORKING_DIRECTORY ${torch_dir})
+    file(REMOVE ${torch_dir}/torch_cc.tar.bz2)
+  else()  # Not M1
+    if (UNIX AND NOT APPLE)  # Linux
+      set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${torch_version}%2Bcpu.zip")
+    elseif (UNIX AND APPLE)  # OSX
+      set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip")
+    else()                   # Windows
+      set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip")
+    endif()
+    file(DOWNLOAD
+      ${torch_url}
+      ${torch_dir}/torch_cc.zip
+      SHOW_PROGRESS
+    )
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf torch_cc.zip WORKING_DIRECTORY ${torch_dir})
+    file(REMOVE ${torch_dir}/torch_cc.zip)
   endif()
-
-  file(DOWNLOAD
-    ${torch_url}
-    ${torch_dir}/torch_cc.zip
-    SHOW_PROGRESS
-  )
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf torch_cc.zip
-                  WORKING_DIRECTORY ${torch_dir})
-
-  file(REMOVE ${torch_dir}/torch_cc.zip)
-
 endif()
 
 # Find the libraries again
